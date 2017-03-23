44918a2f1fd70a36dbe6c7d5982e80087d7aa843
Kari Vänttinen <kvanttinen@gmail.com>
2017-03-23 10:03:11 +0200

    dokumentaatio viikko2

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,2 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (kayttajanimi, salasana) VALUES ('Kari', 'testi');
+INSERT INTO Resepti (kayttaja_id, nimi, kategoria, kuvaus, ohje, valm_aika, annoksia) VALUES (1, 'lihapullat', 2, 'lihapullat sikanaudsta, on herkkua', 'sekoita aineet, pyörittele pullat ja paista uunissa', 60, 5);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,43 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  id SERIAL PRIMARY KEY,
+  kayttajanimi varchar(20) NOT NULL,
+  salasana varchar(20) NOT NULL
+);
+
+CREATE TABLE Resepti(
+  id SERIAL PRIMARY KEY,
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  nimi varchar(50) NOT NULL,
+  kategoria INTEGER NOT NULL,
+  kuvaus varchar(200) NOT NULL,
+  ohje varchar(2000) NOT NULL,
+  valm_aika INTEGER NOT NULL,
+  annoksia INTEGER NOT NULL
+);
+
+CREATE TABLE Suosikit(
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  resepti_id INTEGER REFERENCES Resepti(id)
+);
+
+CREATE TABLE Raaka_aineet(
+  id SERIAL PRIMARY KEY,
+  nimi varchar(100)
+);
+
+CREATE TABLE Ainekset(
+  resepti_id INTEGER REFERENCES Resepti(id),
+  raaka_aine_id INTEGER REFERENCES Raaka_aineet(id),
+  raaka_aine_nimi varchar(50),
+  mittayksikko varchar(15) NOT NULL,
+  maara decimal(4,3) NOT NULL
+);
+
+CREATE TABLE Ravintoarvot(
+  raaka_aine_id INTEGER REFERENCES Raaka_aineet(id),
+  energia decimal(7,6),
+  proteiini decimal(7,6),
+  hiilarit decimal(7,6),
+  rasva decimal(7,6),
+  kuidut decimal(7,6)
+);
