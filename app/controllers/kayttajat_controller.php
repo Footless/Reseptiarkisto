26babcc67ea64dc4bf04d61126d009ffb7eb0f50
Kari Vänttinen <kvanttinen@gmail.com>
2017-03-31 00:48:42 +0300

    ei voi rekisteröityä olemassa olevalla käyttäjätunnuksella, salasanojen täytyy täsmätä

--- app/controllers/kayttajat_controller.php
+++ app/controllers/kayttajat_controller.php
@@ -8,13 +8,21 @@
 
     public static function tallenna(){
       $params = $_POST;
-      $kayttaja = new Kayttaja(array(
-        'kayttajanimi' => $params['kayttajanimi'],
-        'salasana' => $params['salasana']
-      ));
+      $query = DB::connection()->prepare('SELECT * FROM Kayttaja WHERE kayttajanimi = :kayttajanimi LIMIT 1');
+      $query->execute(array('kayttajanimi' => $params['kayttajanimi']));
+      $row = $query->fetch();
+      if ($_POST['salasana'] !== $_POST['salasana_toisto']) {
+        Redirect::to('/kayttajat/rekisteroidy', array('message' => 'Rekisteröityminen epäonnistui! Tarkista salasana.'));
+      } else if ($row) {
+        Redirect::to('/kayttajat/rekisteroidy', array('message' => 'Rekisteröityminen epäonnistui! Valitsemasi käyttäjänimi on jo käytössä.'));
+      } else {
+        $kayttaja = new Kayttaja(array(
+          'kayttajanimi' => $params['kayttajanimi'],
+          'salasana' => $params['salasana']
+        ));
 
-      $kayttaja->save();
-
+        $kayttaja->save();
+      }
       Redirect::to('/', array('message' => 'Rekisteröityminen onnistui!'));
     }
   }
